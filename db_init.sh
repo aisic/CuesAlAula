f963a1be04a54b14fa9e2c050e201403fca0af6f
Isaac Gonzalo <isaac.gonzalo@gmail.com>
2026-08-24 17:27:23 +0200

    fix: Update db_init.sh to use dynamic database credentials for improved flexibility

--- db_init.sh
+++ db_init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "Executant script d'inicialització de l'administrador..."
+echo DB_PASSWORD: $DB_PASSWORD
+echo DB_USER: $DB_USER
+echo DB_NAME: $DB_NAME
 
-mariadb -u root -proot<<-EOSQL
-    USE gestion_colas;
+mariadb -u "${DB_USER:-root}" -p"${DB_PASSWORD}" "$DB_NAME" <<-EOSQL    USE gestion_colas;
     INSERT INTO profesores (nombre, email, password) 
     VALUES ('Admin', '${ADMIN_EMAIL:-isaac.gonzalo@itb.cat}', 'OAUTH_LOGIN')
     ON DUPLICATE KEY UPDATE id=id;
