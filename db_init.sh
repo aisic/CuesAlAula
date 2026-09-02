83314b2b12d928aa7d224bf165bc1856111126de
Isaac Gonzalo <isaac.gonzalo@gmail.com>
2026-09-02 15:37:41 +0200

    fix: Remove sensitive certificate and key files from the repository and integrate LetsEncrypt Certificate

--- db_init.sh
+++ db_init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 echo "Executant script d'inicialització de l'administrador..."
-echo DB_PASSWORD: $MARIADB_ROOT_PASSWORD
-echo DB_USER: $MARIADB_USER
-echo DB_NAME: $MARIADB_DATABASE
 
 mariadb -u "${MARIADB_USER:-root}" -p"${MARIADB_ROOT_PASSWORD}" "$MARIADB_DATABASE" <<-EOSQL
     INSERT INTO profesores (nombre, email, password) 
